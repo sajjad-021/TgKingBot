52138ea10d51243b13d2960c1e033d28afcfdd57
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-03-24 17:39:22 +0430

    king

---
+++ launch.sh
@@ -0,0 +1,53 @@
+ver="1.0"
+if [ "$1" = "--help" ]; then
+  echo -e "
+       \033[01;31m tgKINGbot Telegram Bot \033[0m 
+        \033[01;33m By\033[0m \033[01;30msajjad_021 \033[0m
+        \033[01;34m --help : Show This Msg \033[0m
+        \033[01;34m --about : Show Bot Information \033[0m
+        \033[01;34m --Update : Sync Bot With Master Repo \033[0m"
+
+
+elif [ "$1" = "--about" ]; then
+  echo -e "
+         \033[01;31m tgKing Telegram Bot \033[0m 
+        \033[01;33m By\033[0m \033[01;34msajjad_021 \033[0m
+              \033[01;30m $ver \033[0m
+\033[01;31m A Fun Telegram Bot Written In PY\033[0m"
+
+
+
+elif [ "$1" = "install" ]; then
+  sudo pip install pyTelegramBotAPI
+  sudo pip install telebot
+  sudo pip install logging
+  sudo apt-get install python2.7
+  git clone https://github.com/tgMember/tgKINGbot.git
+  cd tgKINGbot
+
+
+elif [ "$1" = "fix" ]; then
+  cd $HOME
+  sudo pip install pyTelegramBotAPI
+  git clone https://github.com/eternnoir/pyTelegramBotAPI.git
+  cd pyTelegramBotAPI
+  sudo python setup.py install
+  pip install pytelegrambotapi --upgrade
+  cd $HOME
+  cd tgKINGbot
+
+
+elif [ "$1" = "--Update" ]; then
+  git pull
+  echo -e "\033[01;31m Bot Successful Update \033[0m"
+else
+  echo -e "
+        \033[01;31m tgKINGbot Telegram Bot \033[0m 
+        \033[01;33m By\033[0m \033[01;34msajjad_021 \033[0m
+              \033[01;30m $ver \033[0m"
+
+  while true; do
+	nohup python2.7 bot.py
+	sleep 0.5
+  done
+fi
